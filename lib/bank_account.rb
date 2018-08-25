9ef1924b0fdf2bd4eccf3c1117ab88607779c322
Elizabeth Karst <lkarst@outlook.com>
2018-08-25 21:55:01 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,10 +2,12 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
 
-
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
+  end
+
+  def deposit(amount,account)
   end
 end
