8915559edc0a7d8e6c960f444bf4a76199fc06f8
Elizabeth Karst <lkarst@outlook.com>
2018-08-25 22:03:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if transfer.valid?
+    if self.valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
