f762b08bd0bd797c74e16488a4191c866dfd3a4d
Elizabeth Karst <lkarst@outlook.com>
2018-08-25 22:03:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,9 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
